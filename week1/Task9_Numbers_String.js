0f4f07ae85e14057b4962896722dc61bcec72633
Adarsh <adarsh.patel@bacancy.com>
2024-02-05 13:24:38 +0530

    Adds:Numbers and string in JS

---
+++ week1/Task9_Numbers_String.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable prefer-template */
+const Age = 21;
+const Name = 'Adarsh';
+const isStudent = true;
+
+const greeting = 'Namaste! ' + Name;
+console.log(greeting);
+
+const ageString = String(Age);
+console.log('Age is :', ageString);
+
+console.log('Is a student ?', isStudent);
