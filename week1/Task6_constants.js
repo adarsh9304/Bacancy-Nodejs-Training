8ade72e25eb1cf98c70b70133866f93fb0174943
Adarsh <adarsh.patel@bacancy.com>
2024-02-05 12:38:33 +0530

    Adds:constant variable in JS

---
+++ week1/Task6_constants.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const x = 5;
+console.log(x); // 5
+
+// const y;
+// y=5;
+// const variable must be initialized at the time of declaration
+
+// x=7 you can not modified the const varibale
+
+// we can use const variable when we want immuatble behaviour of variable and the value of that variable remains constant throughout whole the program
+
+const URL_GOOGLE = 'www.google.com';
+console.log(URL_GOOGLE);
