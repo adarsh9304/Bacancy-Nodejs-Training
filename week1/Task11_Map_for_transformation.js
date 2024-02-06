39317237dd3f31323da9dff022291250b3cf2965
Adarsh <adarsh.patel@bacancy.com>
2024-02-06 16:58:08 +0530

    Adds:Map for Transformation

---
+++ week1/Task11_Map_for_transformation.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+const arr = [1, 2, 3, 4, 5];
+
+const newArr = arr.map((ele) => ele * ele);
+
+console.log('original array ', arr);
+console.log('New Transformed array', newArr);
