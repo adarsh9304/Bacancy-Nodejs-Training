5d2fb4ffe354102e9961d23c78c5f5b4fd6d5452
Adarsh <adarsh.patel@bacancy.com>
2024-02-20 10:16:48 +0530

    Adds:HTTP status code

---
+++ week4/Task4_HTTP_status_code.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+
+
+/* status_code  meaning
+
+   100-199     Informational Responses
+   200-299      Successful Response
+   300-399      Redirection message
+   400-499      client error message
+   500-599      server error message
+   
+   100      continue
+   102      processing
+   
+   200      OK
+   201      created
+   202      accepted
+   203      non-authorized information
+   204      no content
+   205      reset content
+   206      partial content
+   207      multi status
+   208      already reported
+
+   300      multiple choice
+   301      moved permanently
+   302      found
+   304      not modified
+   305      use proxy
+   306      unused
+   307      temporary Redirect
+   308      permanent Redirect
+
+   400      bad request
+   401      unauthorized
+   402      payment required
+   403      forbidden
+   404      not found
+   405      method not allowed
+   406      not acceptable
+   407      proxy authentication required
+   408      request timeout 
+
+   500      internal server error
+   501      not implemented
+   502      bad gateways
+   503      service unavailable
+   504      gateway timeout
+*/
